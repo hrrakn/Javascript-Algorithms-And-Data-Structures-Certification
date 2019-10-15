f05c023f17ebdc50b1f16dbe0751ea384176c08b
ami.n <i.ami.nakagawa@globis.co.jp>
2019-10-15 19:24:25 +0900

    Create useTheConditionalOperator.js

---
+++ Basic-Javascript/useTheConditionalOperator.js
@@ -0,0 +1,13 @@
+function checkEqual(a, b) {
+    return a === b ? true : false;
+}
+
+checkEqual(1, 2);
+
+// function checkEqual(a, b) {
+    // if (a === b) {
+        // return true;
+    // } else {
+        // return false;
+    // }
+// }
