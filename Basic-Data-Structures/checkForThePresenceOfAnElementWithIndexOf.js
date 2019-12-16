598fba76cfaf849e5103ec5e65cff5476a016286
ami.n <i.ami.nakagawa@globis.co.jp>
2019-12-16 11:43:34 +0900

    update checkForThePresenceOfAnElementWithIndexOf.js

--- Basic-Data-Structures/checkForThePresenceOfAnElementWithIndexOf.js
+++ Basic-Data-Structures/checkForThePresenceOfAnElementWithIndexOf.js
@@ -1,12 +1,37 @@
+// Solution1
 function quickCheck(arr, elem) {
-  // change code below this line
+    // change code below this line
 
     if (arr.indexOf(elem) === -1) {
-      return false;
+        return false;
     } else {
-      return true;
+        return true;
     };
-  // change code above this line
+    // change code above this line
+}
+
+// change code here to test different cases:
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+//Solution 2
+function quickCheck(arr, elem) {
+    // change code below this line
+
+    return arr.indexOf(elem) >= 0 ? true : false;
+
+    // change code above this line
+}
+
+// change code here to test different cases:
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+// Solution 3
+function quickCheck(arr, elem) {
+    // change code below this line
+
+    return arr.indexOf(elem) != -1;
+
+    // change code above this line
 }
 
 // change code here to test different cases:
