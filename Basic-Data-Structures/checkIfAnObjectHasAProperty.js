d9402934fbea537a8e934a8fa2f49dd1c7ccb576
ami.n <i.ami.nakagawa@globis.co.jp>
2019-12-17 18:55:47 +0900

    update checkIfAnObjectHasAProperty.js

---
+++ Basic-Data-Structures/checkIfAnObjectHasAProperty.js
@@ -0,0 +1,29 @@
+let users = {
+    Alan: {
+        age: 27,
+        online: true
+    },
+    Jeff: {
+        age: 32,
+        online: true
+    },
+    Sarah: {
+        age: 48,
+        online: true
+    },
+    Ryan: {
+        age: 19,
+        online: true
+    }
+};
+
+function isEveryoneHere(obj) {
+    // change code below this line
+    return users.hasOwnProperty('Alan') &&
+        users.hasOwnProperty('Jeff') &&
+        users.hasOwnProperty('Sarah') &&
+        users.hasOwnProperty('Ryan') ? true : false;
+    // change code above this line
+}
+
+console.log(isEveryoneHere(users));
