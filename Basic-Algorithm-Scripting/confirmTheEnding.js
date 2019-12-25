939b4cac5c5a64de4aafe99b78b8360443a45b88
ami.n <i.ami.nakagawa@globis.co.jp>
2019-12-25 22:22:29 +0900

    create confirmTheEnding.js

---
+++ Basic-Algorithm-Scripting/confirmTheEnding.js
@@ -0,0 +1,11 @@
+function confirmEnding(str, target) {
+    // "Never give up and good luck will find you."
+    // -- Falcor
+    if (str.substr(-target.length) === target) {
+        return true
+    } else {
+        return false
+    }
+}
+
+confirmEnding("Open sesame", "pen");
